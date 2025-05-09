9194f0f39f4618f87f35fb242c081ecbc497a86b
Crejenovschi David <crejenoriginal.david@gmail.com>
2025-05-09 14:28:24 +0300

    Update DRUM_MINIM_PONDERAT_POZITIV_NEGATIV.cpp

--- GRAPHS/DRUM_MINIM_PONDERAT_POZITIV_NEGATIV.cpp
+++ GRAPHS/DRUM_MINIM_PONDERAT_POZITIV_NEGATIV.cpp
@@ -9,19 +9,37 @@ ofstream fout("result.txt");
 int nr_noduri;
 int nod_start;
 int matrice[101][101];
+int sursa[101];
 
+void afiseaza_drum(int nod) {
+
+	if (nod == nod_start) {
+		fout << nod_start << " ";
+		return;
+	}
+
+	if (sursa[nod] == -1) {
+		fout << "Nu exista "<<endl;
+		return;
+	}
+
+	afiseaza_drum(sursa[nod]); 
+	fout << nod << " ";
+}
 
 void BellmanFord() {
 
 	int distante[101];
 	for (int i = 1; i <= nr_noduri; i++) {
 		distante[i] = INT_MAX;
+		sursa[i] = -1;
 	}
 	distante[nod_start] = 0;
 
 	for (int i = 1; i <= nr_noduri; i++) {
 		if (matrice[nod_start][i] != INT_MAX) {
 			distante[i] = matrice[nod_start][i];
+			sursa[i] = nod_start;
 		}
 	}
 
@@ -30,12 +48,13 @@ void BellmanFord() {
 	for (int i = 2; i <= nr_noduri; i++) {
 		oprire = true;
 		for (int j = 1; j <= nr_noduri; j++) {
-			if (distante[j] != INT_MAX) 
+			if (distante[j] != INT_MAX)
 			{
-				for (int k = 1; k <= nr_noduri; k++) 
+				for (int k = 1; k <= nr_noduri; k++)
 				{
 					if (matrice[j][k] != INT_MAX && matrice[j][k] + distante[j] < distante[k]) {
 						distante[k] = matrice[j][k] + distante[j];
+						sursa[k] = j;
 						oprire = false;
 					}
 				}
@@ -48,6 +67,14 @@ void BellmanFord() {
 
 	for (int i = 1; i <= nr_noduri; i++) {
 		fout << distante[i] << " ";
+	}
+	fout << endl;
+
+	for (int i = 1; i <= nr_noduri; i++) {
+
+		fout << "Drumul de la nodul " << nod_start << " catre " << i << " este : ";
+		afiseaza_drum(i);
+		fout << endl;
 	}
 
 }
