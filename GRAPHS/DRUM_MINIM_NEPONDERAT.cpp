93bd31a2a72c2c307d6a3f59e5b38b983a78b26a
Crejenovschi David <crejenoriginal.david@gmail.com>
2025-05-09 13:58:53 +0300

    Update DRUM_MINIM_NEPONDERAT.cpp

--- GRAPHS/DRUM_MINIM_NEPONDERAT.cpp
+++ GRAPHS/DRUM_MINIM_NEPONDERAT.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <queue>
+#include <vector>
 using namespace std;
 //fisierul contine nr de varfuri, nr de muchii, muchiile, nr de perechi de varfuri la care
 //vrem sa le aflam distantele si varfurile propriu-zise
@@ -17,11 +18,21 @@ int matrice[101][101];
 
 int DrumMinim(int start, int final) {
 
+	if (start <= 0 || final <= 0) {
+		out << "ERROARE! ";
+		return -999;
+	}
+
 	int distante[101]{ 0 };
 	queue<int> coada;
 	distante[start] = 1;
 	coada.push(start);
-	while(!coada.empty()) {
+
+	//Pentru a afisa drumul explicit :
+	int parinte[101]{ 0 };
+	parinte[start] = -1;
+
+	while (!coada.empty()) {
 
 		int nod = coada.front();
 		coada.pop();
@@ -31,14 +42,28 @@ int DrumMinim(int start, int final) {
 			if (matrice[nod][i] == 1 && distante[i] == 0) {
 				coada.push(i);
 				distante[i] = distante[nod] + 1;
+				parinte[i] = nod;
 			}
 		}
 	}
-	
+
 	if (distante[final] == 0) {
 		return -1;
 	}
 	else {
+
+		vector<int>drum;
+		int nod = final;
+		while (nod != -1) {
+			drum.push_back(nod);
+			nod = parinte[nod];
+		}
+
+		out << "Am gasit un drum intre " << start << " si " << final << " : " << endl;
+		for (int i = (int)drum.size() - 1; i >= 0; i--) {
+			out << drum[i] << " ";
+		}
+		out << endl;
 		return distante[final] - 1;
 	}
 
@@ -49,7 +74,7 @@ int main() {
 	fin >> nr_noduri;
 	fin >> nr_muchii;
 
-	for (int i = 1,x,y; i <= nr_muchii; i++) {
+	for (int i = 1, x, y; i <= nr_muchii; i++) {
 
 		fin >> x >> y;
 		matrice[x][y] = matrice[y][x] = 1;
@@ -61,10 +86,15 @@ int main() {
 	for (int i = 1, x, y; i <= k; i++) {
 
 		fin >> x >> y;
-		out << "Distanta minima intre nodurile " << x << " - " << y << " este " << DrumMinim(x, y);
+		int drumMinim = DrumMinim(x,y);
+		if (drumMinim != -999) {
+			out << "Distanta minima intre nodurile " << x << " - " << y << " este " << drumMinim << endl;
+		}
+		else {
+			out << "Nodurile nu au fost date corespunzator !" << endl;
+		}
 		out << endl;
 	}
-	
+
 	return 0;
 }
-
